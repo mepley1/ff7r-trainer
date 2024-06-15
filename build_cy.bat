d080ddeec623df36b2368bf4b0f5adb00b850d6b
rogueautomata <47926856+mepley1@users.noreply.github.com>
2024-06-15 18:10:13 -0500

    Build gui module (cythonized)

--- build_cy.bat
+++ build_cy.bat
@@ -1,7 +1,14 @@
+@echo off
+
+echo Moving any Cython built modules from root dir to /app ...
+move ".\*.*.pyd" ".\app"
+
+echo Beginning build ...
+
 pyinstaller --onefile ^
     --clean ^
     --noconsole ^
-    --add-data=".\assets\ff7r.png;." ^
-    --icon=".\assets\icon.ico" ^
-    --name="TRAINER" ^
+    --add-data=".\assets\ff7r.png;assets" ^
+    --icon=".\assets\icon-p2.ico" ^
+    --name="TRAiNER" ^
     .\app\launch.py
--- setup.py
+++ setup.py
@@ -6,13 +6,28 @@ Usage: python setup.py build_ext --inplace
 from setuptools import setup
 from Cython.Build import cythonize
 
+NAME = 'FF7R Trainer'
+VERSION = '0.1'
+DESC = 'FF7 Remake Cheat Trainer'
+URL = 'https://github.com/mepley1/ff7r-trainer'
+AUTHOR = 'RogueAutomata'
+EMAIL = 'rogueautomata@mepley.net'
+
+SRC_DIR = 'app'
+PACKAGES = [SRC_DIR]
+
 setup(
-    name='FF7R Trainer',
-    description='FF7 Remake Trainer',
+    packages=PACKAGES,
     ext_modules=cythonize(
         'app/gui.pyx',
         compiler_directives={'language_level' : "3"},
     ),
-    #zip_safe=False,
+    zip_safe=False,
     include_package_data=True,
+    name=NAME,
+    description=DESC,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
 )
