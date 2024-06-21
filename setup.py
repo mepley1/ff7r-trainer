a92a907d4e587a928dd96593bcd3bf2c3833878a
rogueautomata <47926856+mepley1@users.noreply.github.com>
2024-06-21 06:22:26 -0500

    Convert from setup.py to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,33 +1,9 @@
-"""
-(Cython) Build gui module. 
-Usage: python setup.py build_ext --inplace
-"""
-
 from setuptools import setup
 from Cython.Build import cythonize
 
-NAME = 'FF7R Trainer'
-VERSION = '0.1'
-DESC = 'FF7 Remake Cheat Trainer'
-URL = 'https://github.com/mepley1/ff7r-trainer'
-AUTHOR = 'RogueAutomata'
-EMAIL = 'rogueautomata@mepley.net'
-
-SRC_DIR = 'app'
-PACKAGES = [SRC_DIR]
-
 setup(
-    packages=PACKAGES,
     ext_modules=cythonize(
-        'app/gui.pyx',
-        compiler_directives={'language_level' : "3"},
+        ['app/gui.py', 'app/settings.py', 'app/offsets.py'],
     ),
-    zip_safe=False,
-    include_package_data=True,
-    name=NAME,
-    description=DESC,
-    version=VERSION,
-    author=AUTHOR,
-    author_email=EMAIL,
-    url=URL,
+
 )
